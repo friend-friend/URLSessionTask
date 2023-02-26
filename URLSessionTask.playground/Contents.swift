9415419124a99df1e847b24069a746842bd7e354
Vyacheslav Onuchin <sla651@mail.ru>
2023-02-26 21:15:48 +1000

    Added the NetworkError enumeration. Added the method.

--- URLSessionTask.playground/Contents.swift
+++ URLSessionTask.playground/Contents.swift
@@ -1,15 +1,23 @@
 import UIKit
 
+enum NetworkError: Error {
+    case connectionTimeout
+    case serverError
+    case networkUnavailable
+    case inavalidURL
+}
+
 func getData(urlRequest: String) {
     let urlRequest = URL(string: urlRequest)
     guard let url = urlRequest else { return }
     URLSession.shared.dataTask(with: url) { data, response, error in
         if error != nil {
-
+            print(NetworkError.serverError.localizedDescription)
         } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+            print(response.statusCode)
             guard let data = data else { return }
             let dataAsString = String(data: data, encoding: .utf8)
-
+            print(dataAsString)
         }
     } .resume()
 }
