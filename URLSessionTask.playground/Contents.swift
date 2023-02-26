a0066734511d89f12ced268840ea2fab9e8ae887
Vyacheslav Onuchin <sla651@mail.ru>
2023-02-26 22:03:24 +1000

    Added api to the code.

--- URLSessionTask.playground/Contents.swift
+++ URLSessionTask.playground/Contents.swift
@@ -8,16 +8,27 @@ enum NetworkError: Error {
 }
 
 func getData(urlRequest: String) {
-    let urlRequest = URL(string: urlRequest)
+    let urlRequest = URL(string: urlRequest) 
     guard let url = urlRequest else { return }
     URLSession.shared.dataTask(with: url) { data, response, error in
         if error != nil {
-            print(NetworkError.serverError.localizedDescription)
+            print(NetworkError.connectionTimeout.localizedDescription)
         } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
             print(response.statusCode)
             guard let data = data else { return }
             let dataAsString = String(data: data, encoding: .utf8)
-            print(dataAsString)
+            print(dataAsString ?? "")
         }
     } .resume()
 }
+
+let urlApi = "https://cat-fact.herokuapp.com"
+getData(urlRequest: urlApi)
+
+var components = URLComponents()
+components.scheme = "https"
+components.host = "cat-fact.herokuapp.com"
+components.path = ""
+
+let urlComponents = components.string ?? ""
+getData(urlRequest: urlComponents)
